6937115140da5dfc7a0238da14b1c706294434ee
RE800T <brutalman122@gmail.com>
2022-09-16 14:07:46 +0300

    camellia: setup dynamic partition

--- device.mk
+++ device.mk
@@ -6,3 +6,8 @@
 
 # Inherit from vendor if exists
 $(call inherit-product-if-exists, vendor/xiaomi/camellia/camellia-vendor.mk)
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_PACKAGES += \
+    fastbootd
