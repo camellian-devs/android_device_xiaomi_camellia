a239debfab0a99685dbd2d372d7d525b8a554576
RE800T <brutalman122@gmail.com>
2022-09-20 10:33:10 +0300

    camellia: Build android.hardware.camera.device@3.6 interface

--- device.mk
+++ device.mk
@@ -106,7 +106,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.5 \
-    android.hardware.camera.provider@2.6
+    android.hardware.camera.provider@2.6 \
+    android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.6
 
 # Configstore
 PRODUCT_PACKAGES += \
